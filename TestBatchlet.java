da62ae9fb9671a8224b0c7f454806e3ad491983b
Mojammal Hock <41143859+MBHock@users.noreply.github.com>
2019-01-30 09:43:28 +0100

    Add files via upload

---
+++ TestBatchlet.java
@@ -0,0 +1,121 @@
+/*
+ * Copyright (c) 2019 Bundesagentur fuer Arbeit. All Rights Reserved.
+ *
+ */
+package de.ba.egov.batch.processing;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.batch.api.Batchlet;
+import javax.batch.runtime.context.JobContext;
+import javax.inject.Inject;
+import javax.inject.Named;
+
+/**
+ *
+ *
+ * @author <a href="mailto:Mojammal.Hock2@arbeitsagentur.de">Mojammal Hock</a>
+ */
+@Named(value = "TestBatchlet")
+public class TestBatchlet implements Batchlet {
+
+	private static final int BLOCK_SIZE = 2000;
+	private static final String OUT_FILENAME = "outputDirectory";
+	private static final String NUMBER_OF_LINES = "numberOfLines";
+
+	private static final LocalDateTime datetime = LocalDateTime.now();
+	private static final DateTimeFormatter formatter = DateTimeFormatter.ISO_LOCAL_DATE_TIME;
+	private static final DateTimeFormatter filenameformatter = DateTimeFormatter.ofPattern("yyyMMdd_HHmmss_SSS");
+
+	private static final Random random = new Random();
+	private static final Integer lowerBound = 1;
+	private static final Integer higherBound = 100;
+	private BufferedWriter writer = null;
+	private AtomicInteger fileNumber = new AtomicInteger(0);
+
+	@Inject
+	private JobContext jobContext;
+
+	@Inject
+	private Logger logger;
+
+	@Override
+	public String process() throws IOException {
+		String lineNumbers = jobContext.getProperties().getProperty(NUMBER_OF_LINES, "4000");
+		logger.log(Level.INFO, "Write {0} lines in file ...", lineNumbers);
+
+		int counter = 0;
+		int limit = Integer.parseInt(lineNumbers);
+		while (counter < limit) {
+
+			openFile(counter);
+
+			writeData(counter);
+
+			counter++;
+		}
+
+		closeBufferedWriter();
+		logger.log(Level.INFO, "{0} line(s) has been written on file", counter);
+		return "COMPLETED";
+
+	}
+
+	private void writeData(int counter) throws IOException {
+
+		int nextNumber = random.nextInt(higherBound - lowerBound) + lowerBound;
+		String testdata = String.format("%s%n", "");
+		if (nextNumber > 20) {
+			testdata = String.format("%s:   current line number is %d.%n", LocalDateTime.now().format(formatter),
+					counter);
+		}
+
+		writer.write(testdata);
+	}
+
+	private void openFile(int counter) throws IOException {
+		logger.log(Level.FINER, "Count value {0}", counter);
+
+		if (counter == 0) {
+
+			writer = new BufferedWriter(new FileWriter(getOutfilename()));
+
+		} else if (counter % BLOCK_SIZE == 0) {
+			closeBufferedWriter();
+			writer = new BufferedWriter(new FileWriter(getOutfilename()));
+		}
+	}
+
+	private void closeBufferedWriter() throws IOException {
+		if (writer != null) {
+			writer.close();
+		}
+	}
+
+	private synchronized File getOutfilename() {
+		File file = new File(jobContext.getProperties().getProperty(OUT_FILENAME),
+				"sampleout_" + datetime.format(filenameformatter) + ".txt");
+
+		if (file.exists()) {
+			file = new File(jobContext.getProperties().getProperty(OUT_FILENAME),
+					"sampleout_" + datetime.format(filenameformatter) + "_" + fileNumber.incrementAndGet() + ".txt");
+		}
+
+		logger.log(Level.INFO, "The {0} file is created in the configured directory", file.getAbsolutePath());
+		return file;
+	}
+
+	@Override
+	public void stop() throws Exception {
+		// do nothing
+	}
+}
